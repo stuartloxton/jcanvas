42b87668f0b2dd3d05fcb4b7a646b9f1c106856b
Stuart Loxton <stuart@strawberrysoup.co.uk>
2009-02-06 09:59:08 +0000

    Now handles some basic text! For FF3.1+ fillText, FF3- mozDrawText and browsers that have neither, absolute positioned div

--- scripts/jquery.canvas.js
+++ scripts/jquery.canvas.js
@@ -1,11 +1,12 @@
 (function($) {
 	
+	
+	var exCanvas = $('<canvas>')[0]; // Create a blank empty canvas not on the page to test features with.
+	$.support.fillText = $.isFunction(exCanvas.getContext('2d').fillText);
+	$.support.CanvasText = $.browser.mozilla;
+	
 	$.fn.getContext = function() {
-		if(this.data('2dContext')) return;
-		else {
-			this.data('2dContext', this[0].getContext('2d'));
-		}
-		return this.data('2dContext');
+		return this[0].getContext('2d');
 	}
 	
 	
@@ -15,13 +16,33 @@
 		ctx.beginPath();
 		
 		ctx.moveTo(args[0][0],args[0][1]);
-		Array.shift(args);
 		
 		$.each(args, function() {
 			ctx.lineTo(this[0], this[1]);
 		})
 		
 		ctx.fill();
+	}
+	
+	$.fn.fillText = function(text, x, y) {
+		ctx = this.getContext();
+		if($.support.fillText) {
+			ctx.fillText(text, x, y);
+		} else {
+			if($.support.CanvasText) {
+				ctx.translate(x, y);
+				ctx.mozDrawText(text)
+				ctx.translate(-x, -y);
+			} else {
+				var canvasOffset = this.offset();
+				var div = $('<div>'+text+'</div>').insertAfter(this);
+				this.next().css({
+						position: 'absolute',
+						left: canvasOffset.left + x,
+						top: canvasOffset.top + y
+					});
+			}
+		}
 	}
 	
 	
